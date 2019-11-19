4883e299b2664128df98442d60582a6cbc828696
haopengtao <haopt@zjbdos.com>
2019-11-19 21:01:25 +0800

    需求：1：集成网管。2：配置中心

--- nacos-provider/nacos-provideer-service/src/main/java/com/wanbo/nacos/provider/NacosProviderApplication.java
+++ nacos-provider/nacos-provideer-service/src/main/java/com/wanbo/nacos/provider/NacosProviderApplication.java
@@ -1,8 +1,10 @@
 package com.wanbo.nacos.provider;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class NacosProviderApplication {
+
+    @Autowired
+    private ConfigurableApplicationContext configurableApplicationContext;
+
     public static void main(String[] args) {
         SpringApplication.run(NacosProviderApplication.class, args);
     }
@@ -20,5 +26,13 @@ public class NacosProviderApplication {
         public String echo(@PathVariable String message) {
             return "Hello Nacos Discovery " + message;
         }
+
+        @GetMapping("/hi")
+        public Object hi(){
+            String name = configurableApplicationContext.getEnvironment().getProperty("user.name");
+            return name;
+        }
     }
+
+
 }
